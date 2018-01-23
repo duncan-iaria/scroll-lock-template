c87fbd2a42aaefac97096f4b11b35e257cc50255
Duncan Iaria <duncan.iaria@soltech.net>
2018-01-22 22:34:17 -0500

    cleaned up stacknav file

--- src/components/shared/StackNavItem.jsx
+++ src/components/shared/StackNavItem.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { debounce, throttle } from 'lodash';
 
-class StackNavItem extends React.Component {
+class StackNavItem extends React.PureComponent {
     constructor( props ){
         super( props );
 
-        // this.onHandleWheel = this.onHandleWheel.bind( this );
         this.onHandleWheel = debounce( this.onHandleWheel, 50, { leading: true, trailing: false } );
         // this.onHandleWheel = throttle( this.onHandleWheel, 1000, { leading: false, trailing: true } );
     }
@@ -15,13 +14,12 @@ class StackNavItem extends React.Component {
     }
     
     onHandleWheel( scrollDir, orderIndex ){
-        console.log('debounce');
+        // console.log('debounce');
         this.props.handleWheel( scrollDir, orderIndex );
     }
 
     render(){
         const { children, handleWheel, orderIndex } = this.props;
-        // const onHandleWheel = ( tEvent, orderIndex ) => { debounce( handleWheel ), 200 }
         return (
             <div style={ { width: '100%', height: '100%' } } 
                 onWheel={ tEvent => this.onHandleWheel( tEvent.deltaY, orderIndex ) }
@@ -31,15 +29,5 @@ class StackNavItem extends React.Component {
         )
     }
 }
-// handleWheel( tEvent, orderIndex ) 
-// const StackNavItem = ({ children, handleWheel, orderIndex }) => {
-//     return (
-//         <div style={ { width: '100%', height: '100%' } } 
-//             onWheel={ test }
-//         >
-//             { children }
-//         </div>
-//     );
-// }
 
 export default StackNavItem;
