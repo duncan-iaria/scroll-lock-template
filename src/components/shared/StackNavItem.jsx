9260e98b7c897033f2a5df7aada0360a3d540287
SNOODBOOK\MiniSnood <duncaniaria@gmail.com>
2018-01-24 23:48:22 -0500

    added beginnings of touch scrolling

--- src/components/shared/StackNavItem.jsx
+++ src/components/shared/StackNavItem.jsx
@@ -5,7 +5,13 @@ class StackNavItem extends React.PureComponent {
     constructor( props ){
         super( props );
 
+        this.state = {
+            touchStartY: null,
+        }
+
         this.onHandleWheel = debounce( this.onHandleWheel, 50, { leading: true, trailing: false } );
+        this.handleTouch = throttle( this.handleTouch, 128 );        
+        this.handleTouchStart = this.handleTouchStart.bind( this );       
         // this.onHandleWheel = throttle( this.onHandleWheel, 1000, { leading: false, trailing: true } );
     }
 
@@ -13,9 +19,23 @@ class StackNavItem extends React.PureComponent {
         this.onHandleWheel.cancel();
     }
     
-    onHandleWheel( scrollDir, orderIndex ){
+    onHandleWheel( tScrollDir, tOrderIndex ){
         // console.log('debounce');
-        this.props.handleWheel( scrollDir, orderIndex );
+        this.props.handleWheel( tScrollDir, tOrderIndex );
+    }
+
+    handleTouchStart( tTouchPosY ){
+        this.setState({ touchStartY: tTouchPosY });
+    }
+
+    handleTouch( tTouchPosY ){
+        console.log( tTouchPosY );
+        if( this.state.touchStartY - tTouchPosY > 0 ){
+            //going up
+        }
+        else {
+            //going down
+        }
     }
 
     render(){
@@ -23,6 +43,8 @@ class StackNavItem extends React.PureComponent {
         return (
             <div style={ { width: '100%', height: '100%' } } 
                 onWheel={ tEvent => this.onHandleWheel( tEvent.deltaY, orderIndex ) }
+                onTouchStart={ e => { this.handleTouchStart( e.touches[0].pageY ) } }
+                onTouchMove={ e => { this.handleTouch( e.touches[0].pageY ) } }
             >
                 { children }
             </div>
