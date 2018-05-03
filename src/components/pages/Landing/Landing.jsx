357efe8b461b4f5df34950ae34aa8a29d03f570d
Duncan Iaria <duncan.iaria@soltech.net>
2018-05-02 21:30:22 -0400

    added footer text to landing

--- src/components/pages/Landing/Landing.jsx
+++ src/components/pages/Landing/Landing.jsx
@@ -15,25 +15,9 @@ const Landing = () => {
           <img src={logo} alt={'Rhiannon and Duncan Logo'} />
         </AnimatedText>
       </div>
-      {/* <div className="landing__title"> */}
-      {/* <AnimatedText>
-                    Join us for a fall time celebration of love!
-                    <br/>
-                    <br/>                    
-                    10 / 27 / 2018
-                </AnimatedText> */}
-      {/* <img src={logo} alt={'Rhiannon and Duncan Logo'} /> */}
-      {/* </div> */}
-
-      <div className="landing__footer" />
-      {/* <h1>LANDING PAGE</h1>
-            <div>
-                <div className="landing__color-block ecru"></div>
-                <div className="landing__color-block olive"></div>                
-                <div className="landing__color-block ocean"></div>
-                <div className="landing__color-block plum"></div>
-                <div className="landing__color-block oxblood"></div>
-            </div> */}
+      <div className="landing__footer">
+        <div className="landing__footer_text">10.27.2018</div>
+      </div>
     </div>
   );
 };
