f9bcb9b5fd1150125cd24c8422e6ae6f036d9198
Duncan Iaria <duncan.iaria@soltech.net>
2018-05-05 12:21:38 -0400

    added custom favicon, tweaked animations and added some visual flair to the landing footer

--- src/components/pages/Landing/Landing.jsx
+++ src/components/pages/Landing/Landing.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import AnimatedText from '../../shared/AnimatedText/AnimatedText';
 //STYLES
 import './Landing.style.css';
-const logo = require('../../../assets/images/logo/FullFox@2x.png');
+const logo = require('../../../assets/images/logo/FullFox.svg');
+const tree = require('../../../assets/images/logo/Tree.svg');
 
 //=========================
 // COMPONENT
@@ -12,10 +13,11 @@ const Landing = () => {
     <div className="landing__container">
       <div className="landing__title">
         <AnimatedText>
-          <img src={logo} alt={'Rhiannon and Duncan Logo'} />
+          <img src={logo} alt={'Rhiannon and Duncan Logo'} height={500} />
         </AnimatedText>
       </div>
       <div className="landing__footer">
+        <img src={tree} alt={'tree icon'} height={22} />
         <div className="landing__footer_text">10.27.2018</div>
       </div>
     </div>
--- src/components/pages/Story/Story.jsx
+++ src/components/pages/Story/Story.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AnimatedText from '../../shared/AnimatedText/AnimatedText';
 
 // STYLES
 import './Story.style.css';
@@ -16,23 +17,25 @@ const Story = ({ history }) => {
         <h1>Our Story</h1>
       </div>
       <div className="Story__text">
-        <p>
-          Most of you know that Duncan and Rhiannon met while living in Savannah – next door to one another! The story
-          goes that one afternoon, while hard at work, Rhiannon (and Jenny!) had enough of the loud boys next door. They
-          went over to give them a piece of their mind and ended up being completely disarmed by the sweet boy next door
-          (Duncan).
-        </p>
-        <p>
-          Turns out the loud music wasn’t even coming from Duncan’s place, but being the great guy that he is, he wasn’t
-          even offended at the accusation. In fact, he took the opportunity to invite the girls to one of his shows (he
-          was a drummer in General Oglethorpe and the Panhandlers).
-        </p>
-        <p>
-          Rhiannon didn’t make it to this show, but as fate would have it, a few months later, they ran into each other
-          at a popular spot downtime on Cinco de Mayo. The rest, as they say, is history! Along the way, they have lived
-          in Chapel Hill, NC (where they adopted their darling dog Sophie) and now in Atlanta, GA. They can’t wait to
-          celebrate with their family and friends!
-        </p>
+        <AnimatedText>
+          <p>
+            Most of you know that Duncan and Rhiannon met while living in Savannah – next door to one another! The story
+            goes that one afternoon, while hard at work, Rhiannon (and Jenny!) had enough of the loud boys next door.
+            They went over to give them a piece of their mind and ended up being completely disarmed by the sweet boy
+            next door (Duncan).
+          </p>
+          <p>
+            Turns out the loud music wasn’t even coming from Duncan’s place, but being the great guy that he is, he
+            wasn’t even offended at the accusation. In fact, he took the opportunity to invite the girls to one of his
+            shows (he was a drummer in General Oglethorpe and the Panhandlers).
+          </p>
+          <p>
+            Rhiannon didn’t make it to this show, but as fate would have it, a few months later, they ran into each
+            other at a popular spot downtime on Cinco de Mayo. The rest, as they say, is history! Along the way, they
+            have lived in Chapel Hill, NC (where they adopted their darling dog Sophie) and now in Atlanta, GA. They
+            can’t wait to celebrate with their family and friends!
+          </p>
+        </AnimatedText>
       </div>
       <div />
       {/* <div className="Story__footer"/> */}
