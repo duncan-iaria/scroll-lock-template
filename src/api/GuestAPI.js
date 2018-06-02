796ca0a4fe286ba283e4409a9eca6e72264ca2b8
Duncan Iaria <duncaniaria@gmail.com>
2018-06-02 13:01:11 -0400

    wrapped getGuest call in a try/catch

--- src/api/GuestAPI.js
+++ src/api/GuestAPI.js
@@ -3,10 +3,22 @@ const API_URL = 'http://localhost:7071/api/GetGuest';
 export const searchGuests = async tGuestSearch => {
   let tempUrl = new URL(API_URL);
   let tempResponse;
+  const tempHeaders = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*"
+  }
 
   tempUrl.searchParams.append('guestSearch', tGuestSearch);
-  tempResponse = await fetch(tempUrl);
-  tempResponse = await tempResponse.json();
 
+  try {
+    tempResponse = await fetch(tempUrl, { headers: tempHeaders });
+    tempResponse = await tempResponse.json();
+
+  }
+  catch (tError) {
+    console.log(tError);
+  };
+
+  console.log('api response: ', tempResponse)
   return tempResponse;
 };
